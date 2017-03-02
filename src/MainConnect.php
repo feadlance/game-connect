e26aeae8b43a46dff9893cdaa041ac864cc1d7d0
Davut Kember <davutkmbr@gmail.com>
2017-03-02 21:38:58 +0300

    Add comments

--- src/MainConnect.php
+++ src/MainConnect.php
@@ -4,28 +4,83 @@ namespace Weblebby\GameConnect;
 
 class MainConnect
 {
+	/**
+	 * IP address on server.
+	 *
+	 * @var string
+	 */
 	public $ip;
-	public $port;
-	public $status;
-	public $timeout;
-	
-	protected $socket;
-	protected $options;
 
-	public function __construct($ip, $port, $timeout = 2, array $options = [])
+	/**
+	 * Port on server.
+	 *
+	 * @var integer
+	 */
+	public $port = 25565;
+
+	/**
+	 * Server status. online/offline
+	 *
+	 * @var boolean
+	 */
+	public $status = false;
+
+	/**
+	 * Connection timeout.
+	 *
+	 * @var integer
+	 */
+	public $timeout = 2;
+	
+	/**
+	 * Connection socket.
+	 *
+	 * @var mixed
+	 */
+	protected $socket;
+
+	/**
+	 * Config.
+	 *
+	 * @var array
+	 */
+	protected $config;
+
+	/**
+	 * Load the class.
+	 *
+	 * @param string $ip IP address on server.
+	 * @param integer $port Port on server.
+	 * @param integer $timeout Connection timeout.
+	 * @param array $config Config values.
+	 *
+	 * @return void
+	 */
+	public function __construct($ip, $port, $timeout = 2, array $config = [])
 	{
 		$this->ip = $ip;
 		$this->port = $port;
 
-		$this->options = $options;
+		$this->config = $config;
 		$this->timeout = $timeout;
 	}
 
-	public function option($option)
+	/**
+	 * Get config value.
+	 *
+	 * @param string $config Config key.
+	 * @return mixed
+	 */
+	public function config($config)
 	{
-		return isset($this->options[$option]) ? $this->options[$option] : null;
+		return isset($this->config[$config]) ? $this->config[$config] : null;
 	}
 
+	/**
+	 * Close the connection.
+	 *
+	 * @return void
+	 */
 	protected function disconnect()
 	{
 		if ( $this->socket ) {
